629b27333f1c7dba2ffcbd128c81cab858e8c722
Miguel <msaturno2021@gmail.com>
2022-07-04 06:25:37 -0500

    resultados 15

--- __init__.py
+++ __init__.py
@@ -6,56 +6,6 @@ import pymysql
 from datetime import datetime, timedelta
 
 
-req_body = [
-    {
-    "tipo_IoT" : "temperatura",
-    "codigo_IoT" : "T01",
-    "valor_IoT" : "27.01",
-    "medida_IoT" : "C",
-    "fecha_entrada" : "2022-07-04T08:42:34.5220000Z"
-    },
-    {
-    "tipo_IoT" : "temperatura",
-    "codigo_IoT" : "T02",
-    "valor_IoT" : "27.01",
-    "medida_IoT" : "C",
-    "fecha_entrada" : "2022-07-04T08:42:34.5220000Z"
-    }
-]
-for texto in req_body:
-    tipo = texto.get('tipo_IoT')
-    codigo = texto.get('codigo_IoT')
-    valor = texto.get('valor_IoT')
-    medida = texto.get('medida_IoT')
-    fecha_entrada = texto.get('fecha_entrada')
-    now = datetime.now()
-    now = now - timedelta(hours= 5)
-    fecha_salida = now.strftime("%Y-%m-%d %H:%M:%S")
-    fecha_entrada = fecha_entrada = fecha_entrada[0:10] + " " + fecha_entrada[11:19]
-    try:
-        logging.info('Python HTTP trigger function processed a request.')
-        cnx = pymysql.connect(
-        user = "Mhiaghi",
-        password = 'Miguel123',
-        host = 'msaturno-database.mysql.database.azure.com',
-        port = 3306,
-        database = 'SEIDORLAB'
-        )
-        logging.info(cnx)
-        cursor = cnx.cursor()
-        cursor.execute("INSERT INTO devices VALUES ('%s','%s','%s', '%s','%s','%s') " % (tipo,codigo,valor,medida,fecha_entrada,fecha_salida))
-        cursor.execute("CALL eliminarultimasfilas('%s') " % (codigo))
-        cnx.commit()
-        print("introduccion exitoso")
-    except pymysql.IntegrityError:
-        print("Error en base de datos de muchas filas")
-
-
-
-
-
-
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
