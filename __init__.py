09104e1d6375f801b7ce7a1deccb3ed03ed07d2f
Miguel <msaturno2021@gmail.com>
2022-07-04 04:26:38 -0500

    resultados 8

--- __init__.py
+++ __init__.py
@@ -24,11 +24,52 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         medida = req.params.get('medida_IoT')
         fecha_entrada = req.params.get('fecha_entrada')
     except:
-        return func.HttpResponse(
-            "Mensaje Nulo",
-            status_code=200
-        )
-
+        try:
+            req_body = req.get_json()
+            try:
+                for texto in req_body:
+                    tipo = texto.get('tipo_IoT')
+                    codigo = texto.get('codigo_IoT')
+                    valor = texto.get('valor_IoT')
+                    medida = texto.get('medida_IoT')
+                    fecha_entrada = texto.get('fecha_entrada')
+                    now = datetime.now()
+                    now = now - timedelta(hours= 5)
+                    fecha_salida = now.strftime("%Y-%m-%d %H:%M:%S")
+                    fecha_entrada = fecha_entrada = fecha_entrada[0:10] + " " + fecha_entrada[11:19]
+                    try:
+                        cursor.execute("INSERT INTO devices VALUES ('%s','%s','%s', '%s','%s','%s') " % (tipo,codigo,valor,medida,fecha_entrada,fecha_salida))
+                        cursor.execute("CALL eliminarultimasfilas('%s') " % (codigo))
+                        cnx.commit()
+                    except pymysql.IntegrityError:
+                        return func.HttpResponse(
+                        "Error en base de datos",
+                        status_code=200
+                        )
+            except:
+                tipo = req_body.get('tipo_IoT')
+                codigo = req_body.get('codigo_IoT')
+                valor = req_body.get('valor_IoT')
+                medida = req_body.get('medida_IoT')
+                fecha_entrada = req_body.get('fecha_entrada')
+                now = datetime.now()
+                now = now - timedelta(hours= 5)
+                fecha_salida = now.strftime("%Y-%m-%d %H:%M:%S")
+                fecha_entrada = fecha_entrada = fecha_entrada[0:10] + " " + fecha_entrada[11:19]
+                try:
+                    cursor.execute("INSERT INTO devices VALUES ('%s','%s','%s', '%s','%s','%s') " % (tipo,codigo,valor,medida,fecha_entrada,fecha_salida))
+                    cursor.execute("CALL eliminarultimasfilas('%s') " % (codigo))
+                    cnx.commit()
+                except pymysql.IntegrityError:
+                    return func.HttpResponse(
+                    "Error en base de datos",
+                    status_code=200
+                    )
+        except:
+            return func.HttpResponse(
+                "Mensaje Nulo",
+                status_code=200
+                )
 
     if not tipo:
         try:
