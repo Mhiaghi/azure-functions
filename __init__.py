c28786f50477c8c277b857880c656a1c4ea2d409
Miguel <msaturno2021@gmail.com>
2022-07-04 06:32:23 -0500

    resultados 16

--- __init__.py
+++ __init__.py
@@ -10,6 +10,16 @@ from datetime import datetime, timedelta
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python HTTP trigger function processed a request.')
+
+    cnx = pymysql.connect(
+    user = "Mhiaghi",
+    password = 'Miguel123',
+    host = 'msaturno-database.mysql.database.azure.com',
+    port = 3306,
+    database = 'SEIDORLAB'
+    )
+    logging.info(cnx)
+    cursor = cnx.cursor()
     req_body = req.get_json()
     #multiple
     for texto in req_body:
@@ -23,16 +33,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         fecha_salida = now.strftime("%Y-%m-%d %H:%M:%S")
         fecha_entrada = fecha_entrada = fecha_entrada[0:10] + " " + fecha_entrada[11:19]
         try:
-            logging.info('Python HTTP trigger function processed a request.')
-            cnx = pymysql.connect(
-            user = "Mhiaghi",
-            password = 'Miguel123',
-            host = 'msaturno-database.mysql.database.azure.com',
-            port = 3306,
-            database = 'SEIDORLAB'
-            )
-            logging.info(cnx)
-            cursor = cnx.cursor()
+            
             cursor.execute("INSERT INTO devices VALUES ('%s','%s','%s', '%s','%s','%s') " % (tipo,codigo,valor,medida,fecha_entrada,fecha_salida))
             cursor.execute("CALL eliminarultimasfilas('%s') " % (codigo))
             cnx.commit()
