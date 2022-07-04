584156131193c2f6271337f178e0fbae6defe5ae
Miguel <msaturno2021@gmail.com>
2022-07-04 01:35:10 -0500

    resultados 4

--- __init__.py
+++ __init__.py
@@ -39,9 +39,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             now = datetime.now()
             now = now - timedelta(hours= 5)
             fecha_salida = now.strftime("%Y-%m-%d %H:%M:%S")
+            fecha_entrada = fecha_entrada = fecha_entrada[0:10] + " " + fecha_entrada[11:19]
             cursor.execute("INSERT INTO devices VALUES ('%s','%s','%s', '%s','%s','%s') " % (tipo,codigo,valor,medida,fecha_entrada,fecha_salida))
             cursor.execute("CALL eliminarultimasfilas('%s') " % (codigo))
-            fecha_entrada = fecha_entrada = fecha_entrada[0:10] + " " + fecha_entrada[11:19]
+            
             cnx.commit()
         except pymysql.IntegrityError:
             print("Error")
