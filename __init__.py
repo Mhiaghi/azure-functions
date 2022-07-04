f9ccb73589a3f2449d2c97c34b6704970a56517a
Miguel <msaturno2021@gmail.com>
2022-07-04 05:23:50 -0500

    resultados 11

--- __init__.py
+++ __init__.py
@@ -10,6 +10,54 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     try:
         req_body = req.get_json()
+        #multiple
+        try:
+            for texto in req_body:
+                pass
+        #uno solo
+        except:
+            try:
+                tipo = req_body.get('tipo_IoT')
+                codigo = req_body.get('codigo_IoT')
+                valor = req_body.get('valor_IoT')
+                medida = req_body.get('medida_IoT')
+                fecha_entrada = req_body.get('fecha_entrada')
+                now = datetime.now()
+                now = now - timedelta(hours= 5)
+                fecha_salida = now.strftime("%Y-%m-%d %H:%M:%S")
+                fecha_entrada = fecha_entrada = fecha_entrada[0:10] + " " + fecha_entrada[11:19]
+                try:
+                    logging.info('Python HTTP trigger function processed a request.')
+                    cnx = pymysql.connect(
+                        user = "Mhiaghi",
+                        password = 'Miguel123',
+                        host = 'msaturno-database.mysql.database.azure.com',
+                        port = 3306,
+                    database = 'SEIDORLAB'
+                    )
+                    logging.info(cnx)
+                    cursor = cnx.cursor()
+                    cursor.execute("INSERT INTO devices VALUES ('%s','%s','%s', '%s','%s','%s') " % (tipo,codigo,valor,medida,fecha_entrada,fecha_salida))
+                    cursor.execute("CALL eliminarultimasfilas('%s') " % (codigo))
+                    cnx.commit()
+                    return func.HttpResponse(
+                    "Exito al ingresar una sola fila {codigo}",
+                    status_code=200
+                    )
+                except pymysql.IntegrityError:
+                    return func.HttpResponse(
+                    "Error en base de datos",
+                    status_code=200
+                    )
+            except:
+                return func.HttpResponse(
+                    "Error al recibir una sola fila",
+                    status_code=200
+                    )
+            return func.HttpResponse(
+                "Se recibio una sola fila",
+                status_code=200
+                )
         return func.HttpResponse(
                 "Se recibio un json",
                 status_code=200
