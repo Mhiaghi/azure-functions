ce02478b03f0bc27ed31ef5a43db8bc72b376533
Miguel <msaturno2021@gmail.com>
2022-07-04 05:10:22 -0500

    resultados 10

--- __init__.py
+++ __init__.py
@@ -8,7 +8,14 @@ from datetime import datetime, timedelta
 def main(req: func.HttpRequest) -> func.HttpResponse:
 
     logging.info('Python HTTP trigger function processed a request.')
-    return func.HttpResponse(
-        "Se recibio un mensaje",
-        status_code=200
-        )
+    try:
+        req_body = req.get_json()
+        return func.HttpResponse(
+                "Se recibio un json",
+                status_code=200
+                )
+    except:
+        return func.HttpResponse(
+                "Mensaje Nulo",
+                status_code=200
+                )
